4c2d10138483218fb3a631edc853df0b5195b36a
Randula1997 <randularj97@gmail.com>
2023-09-27 19:37:00 +0530

    initial project setup

---
+++ Controllers/doctorController.js
@@ -0,0 +1,93 @@
+import Doctor from "../models/DoctorSchema.js";
+
+export const updateDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const updatedDoctor = await Doctor.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true }
+    ).select("-password");
+    res.status(200).json({
+      success: true,
+      message: "Successfully Updated",
+      data: updatedDoctor,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Update Unsuccessfull",
+    });
+  }
+};
+
+export const deleteDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Doctor.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully Deleted",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "Delete Unsuccessfull",
+    });
+  }
+};
+
+export const getSingleDoctor = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const doctor = await Doctor.findById(id)
+      .populate("reviews")
+      .select("-password");
+    res.status(200).json({
+      success: true,
+      message: "Doctor Found",
+      data: doctor,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "No Doctor found",
+    });
+  }
+};
+
+export const getAllDoctors = async (req, res) => {
+  try {
+    const { query } = req.query;
+    let doctors;
+
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else {
+      doctors = await Doctor.find({ isApproved: "approved" }).select(
+        "-password"
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Doctors Found",
+      data: doctors,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "No Doctors found",
+    });
+  }
+};
